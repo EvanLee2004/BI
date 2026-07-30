cbc3dec6cb7193217d40c2b27ba7bb6a78e18c8d
EvanLee2004 <lmh20251202@gmail.com>
2026-07-30 14:54:50 +0800

    build(3.4.0): 打包看端 dist（含重点客户面板）
    生产 nginx 只服务 frontend/dist；源码 pull 后必须重编并上机，否则用户看不到 3.4.0 UI。

--- frontend/dist/assets/ExpenseHeatmap-BkuHH55h.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./ExpenseHeatmap.vue_vue_type_script_setup_true_lang-DrydmqXF.js";import"./boot-cockpit-vAUl6Xs4.js";import"./vue-runtime-C3pNuKTs.js";import"./index-heaQqad_.js";import"./theme-BH0sxN4-.js";import"./echarts-loader-BzFImTki.js";export{o as default};
--- frontend/dist/assets/LedgerTable-CXlSRpkJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./LedgerTable.vue_vue_type_script_setup_true_lang-Pp0ZTIcW.js";import"./vue-runtime-C3pNuKTs.js";import"./boot-cockpit-vAUl6Xs4.js";import"./index-heaQqad_.js";import"./theme-BH0sxN4-.js";import"./echarts-loader-BzFImTki.js";export{o as default};
